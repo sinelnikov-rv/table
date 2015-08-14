2216c126a4b9990c5ee36567cd1e9c1c78d473ef
Sinelnikov Roman <sinelnikov.rv@gmail.com>
2015-08-14 11:39:49 +0300

    create table

---
+++ table.js
@@ -0,0 +1,21 @@
+window.onload = function() { init() };
+
+var public_spreadsheet_url = '1TIF8w8uAB-iigH9fUHhjX6Vgc_0KEDD4ZdFlSDope38';
+
+function init() {
+    Tabletop.init( { key: public_spreadsheet_url,
+        callback: showInfo,
+        simpleSheet: true } )
+}
+function showInfo(data, tabletop) {
+    //alert("Successfully processed!")
+    for (j in data[0]) {
+        $('#title').append('<th>' + j + '</th>');
+    }
+    for (var i = 0; i < data.length; i++) {
+        $('#products').append('<tr id="row' + i + '" ></tr>');
+        for (var n in data[i]) {
+            $('#row' + i).append('<td>' + data[i][n] + '</td>');
+        }
+    }
+}
